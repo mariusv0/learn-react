f5997024b184e0052c80a49ccec3166f17f295c7
Klaus Nielsen <klnie1989@gmail.com>
2019-03-26 21:53:38 +0100

    Added logo and animation

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './style/App.css';
 
 class App extends Component {
@@ -6,6 +7,7 @@ class App extends Component {
     return(
       <div className="App">
         <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
           <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
         </header>
       </div>
