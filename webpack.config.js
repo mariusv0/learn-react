5e47b88e44053be0507fa1601691c426092bddde
Marius <mariusvilholm08@gmail.com>
2019-03-29 13:09:43 +0100

    Favicon + Manifest

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, './src/app/index.js'),
@@ -29,7 +30,7 @@ module.exports = {
         use: ["style-loader", "css-loader"]
       },
       {
-        test: /\.(png|svg|jpg|gif)$/,
+        test: /\.(png|svg|jpg|gif|ico)$/,
         use: [
           'file-loader'
         ]
@@ -47,6 +48,7 @@ module.exports = {
     // clean dist folder
     new CleanWebpackPlugin(['dist'], {
       'verbose': true // write logs to console
-    })
+    }),
+    new CopyPlugin(['./public/manifest.json', './public/favicon.ico']),
   ]
 };
